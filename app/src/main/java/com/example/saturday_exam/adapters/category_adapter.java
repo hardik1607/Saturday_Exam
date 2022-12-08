299a2ebc0b10d6a440ed3fd6040b9dbab79d3efc
HP <hardiklaheri13@gmail.com>
2022-12-08 10:19:33 +0530

    Initial commit

---
+++ app/src/main/java/com/example/saturday_exam/adapters/category_adapter.java
@@ -0,0 +1,44 @@
+package com.example.saturday_exam.adapters;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import com.example.saturday_exam.R;
+import com.example.saturday_exam.activitys.category;
+import com.example.saturday_exam.config;
+
+public class category_adapter extends BaseAdapter {
+
+    category category;
+    public category_adapter(category category) {
+        this.category=category;
+    }
+
+    @Override
+    public int getCount() {
+        return config.category_name.length;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return i;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return i;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        view= LayoutInflater.from(category).inflate(R.layout.category_item,viewGroup,false);
+
+        TextView textView=view.findViewById(R.id.category_text);
+
+        textView.setText(config.category_name[i]);
+        return view;
+    }
+}
